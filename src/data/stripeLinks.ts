6c7f5232652b9fb77261dd8da81f3e588be2daab
Gabriel <gabrielleitemoreira@gmail.com>
2025-11-25 16:19:25 -0300

    inclusao valores strip

---
+++ src/data/stripeLinks.ts
@@ -0,0 +1,15 @@
+export const stripeLinks: Record<string, string> = {
+  "Assinatura Mensal": "https://buy.stripe.com/28EeVf60E2bDgp272p2cg0a",
+  "Assinatura Semestral": "https://buy.stripe.com/7sY14p60EbMd8WA1I52cg0b",
+
+  "Mapa Astral Personalizado": "https://buy.stripe.com/aFa9AVagU7vXgp272p2cg09",
+  "Horóscopo Personalizado": "https://buy.stripe.com/5kQ7sN1KodUl5Ko4Uh2cg08",
+  "Sinastria Amorosa": "https://buy.stripe.com/14AdRbfBe6rTgp24Uh2cg07",
+  "Tarot Direto": "https://buy.stripe.com/28EdRbcp24jL2yc4Uh2cg06",
+  "Tarot Mensal": "https://buy.stripe.com/8x26oJ9cQ17zgp2fyV2cg05",
+  "Numerologia – Mapa do Ano": "https://buy.stripe.com/8x200lbkYaI9a0EgCZ2cg04",
+  "Mapa Profissional": "https://buy.stripe.com/fZueVfgFi03vgp2aeB2cg03",
+  "Mapa Infantil": "https://buy.stripe.com/14AbJ3bkYaI9fkY3Qd2cg02",
+  "Mapa Sexual": "https://buy.stripe.com/4gMeVfbkY5nP6Os9ax2cg01",
+  "Consulta de Bem-Estar / Terapia Leve": "https://buy.stripe.com/28EbJ360E4jLb4IbiF2cg00",
+};
