eca581209c94b9d9b458748545319cf3417b1be8
Elio B Junior <eliobj@gmail.com>
2025-12-04 14:49:57 -0300

    Ajuste final nos links de pagamento para string

--- src/data/stripeLinks.ts
+++ src/data/stripeLinks.ts
@@ -1,69 +1,69 @@
 // src/data/stripeLinks.ts
 export const STRIPE_LINKS: Record<string, any> = {
   "Sinastria Amorosa": {
-    avulso: "https://buy.stripe.com/fZu7sN88MbMd8WA5Y12cg0s",
+    avulso: String.raw `https://buy.stripe.com/fZu7sN88MbMd8WA5Yl2cg0s`,
     precoAvulso: "R$ 19,00",
   },
   "Mapa Astral Personalizado": {
-    avulso: "https://buy.stripe.com/28E6oJ885mP5k0E12cg0g",
+    avulso: String.raw `https://buy.stripe.com/28E6oJ88M5nP5Ko0E12cg0q`,
     precoAvulso: "R$ 14,00",
   },
   "Horóscopo Personalizado": {
-    avulso: "https://buy.stripe.com/aFa6oJcp28A1dcQdqN2cg0g",
+    avulso: String.raw `https://buy.stripe.com/aFa6oJcp28A1dcQdqN2cg0p`,
     precoAvulso: "R$ 14,00",
   },
   "Tarot Direto": {
-    avulso: "https://buy.stripe.com/14A5kF88mOCheqUbfR2cg0o",
+    avulso: String.raw `https://buy.stripe.com/14A5kF88McQhegUbiF2cg0o`,
     precoAvulso: "R$ 14,00",
   },
   "Numerologia – Mapa do Ano": {
-    avulso: "https://buy.stripe.com/8x2dP8RcOeYJc0GA9az2cg0n",
+    avulso: String.raw `https://buy.stripe.com/8x2dRb9cQeYpdcQ9ax2cg0n`,
     precoAvulso: "R$ 14,00",
   },
   "Mapa Infantil": {
-    avulso: "https://buy.stripe.com/Tu3cxgFi7iZtHeYuR2cq0m",
+    avulso: String.raw `https://buy.stripe.com/fZu3cxgFi17zfkYeuR2cg0m`,
     precoAvulso: "R$ 14,00",
   },
   "Mapa Sexual": {
-    avulso: "https://buy.stripe.com/eVq3cxd6b2D5kDo86t2cq0g",
+    avulso: String.raw `https://buy.stripe.com/eVq3cxdt62bD5Ko86t2cg0l`,
     precoAvulso: "R$ 14,00",
   },
   "Tarot Mensal Premium": {
-    avulso: "https://buy.stripe.com/chJkJ94AWI3QcG2M9az2cg0c",
+    avulso: String.raw `https://buy.stripe.com/cNibJ34WAaI93Cg2M92cg0k`,
     precoAvulso: "R$ 19,00",
   },
   "Mapa Profissional": {
-    avulso: "https://buy.stripe.com/9b6BSe33AWc4QhW8tE2cg0i",
+    avulso: String.raw `https://buy.stripe.com/9B6bJ34WAcQh8WA86t2cg0i`,
     precoAvulso: "R$ 19,00",
   },
   "Diagnóstico do Amor": {
-    avulso: "https://buy.stripe.com/bJo0l88M9E5fkYeuR2cq0f",
+    avulso: String.raw `https://buy.stripe.com/bJe00l88M9E5fkYeuR2cg0f`,
     precoAvulso: "R$ 7,00",
   },
   "Missão de Vida 2026": {
-    avulso: "https://buy.stripe.com/9B67s3NswcQh3CgfvY2cq0g",
+    avulso: String.raw `https://buy.stripe.com/9B67sN3SwcQh3CgfyV2cg0e`,
     precoAvulso: "R$ 12,00",
   },
   "Seu Ano em 3 Palavras": {
-    avulso: "https://buy.stripe.com/4gM6oJ40kLjB4IaeB2cg0d",
+    avulso: String.raw `https://buy.stripe.com/4gM6oJ1Ko4jLb4IaeB2cg0d`,
     precoAvulso: "R$ 5,00",
   },
   "Análise Secreta do Seu Signo": {
-    avulso: "https://buy.stripe.com/dRmdRb2Os03v1u8euR2cq0g",
+    avulso: String.raw `https://buy.stripe.com/dRmdRb2Os03v1u8euR2cg0c`,
     precoAvulso: "R$ 7,00",
   },
 
   // PLANOS
   "Plano Total Mensal": {
-    mensal: "https://buy.stripe.com/7sY8wR2OsbMdKY4hU2cq0h",
+    mensal: String.raw `https://buy.stripe.com/7sY8wR2OsbMdfkY4Uh2cg0h`,
     precoMensal: "R$ 29,00/mês",
   },
   "Clube Alma Ramos – O mais completo": {
-    semestral: "https://buy.stripe.com/5kO8wRexa17z3CgbfR2cq0g",
+    semestral: String.raw `https://buy.stripe.com/5kQ8wRexa17z3CgbiF2cg0g`,
     precoSemestral: "R$ 129,00 a cada 6 meses",
   },
   "Terapia de Bem-Estar com I.A": {
-    semestral: "https://buy.stripe.com/8x28wRdt64jLDcQ86t2cq0r",
+    semestral: String.raw `https://buy.stripe.com/8x28wRdt64jLdcQ86t2cg0r`,
     precoSemestral: "R$ 114,00 a cada 6 meses",
   },
 };
