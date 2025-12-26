73a15bf4bb40c8fc88ccc1c4c92bb6089546171a
Elio B Junior <eliobj@gmail.com>
2025-12-26 18:27:00 -0300

    remove Hero section permanently

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { HeaderBeneficiosPro } from "@/components/HeaderBeneficiosPro";
-import { Hero } from "@/components/Hero";
 import { TodosProdutos } from "@/components/TodosProdutos";
 
 function App() {
   return (
     <>
       <HeaderBeneficiosPro />
-      <Hero />
       <TodosProdutos />
     </>
   );
